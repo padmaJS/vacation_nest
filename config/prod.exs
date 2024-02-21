1923b75106c5ddc0d573fd4e3a79e79644b84026
padmaJS <padshak230@gmail.com>
2024-02-21 21:01:11 +0545

    init

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :vacation_nest, VacationNestWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "https://vacation-nest.gigalixirapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
