6d0afef225f1d4ee60aa8eb5fc586c5a03055440
Rijan-Budds <buddsrijan@gmail.com>
2024-05-28 14:44:32 +0545

    resolved mistakes

--- priv/repo/migrations/20240211170539_create_hotels.exs
+++ priv/repo/migrations/20240211170539_create_hotels.exs
@@ -21,7 +21,9 @@ defmodule VacationNest.Repo.Migrations.CreateHotels do
       timestamps()
     end
 
-    create unique_index(:hotels, [:email, :phone_number, :facebook_url, :instagram_url])
-
+      create unique_index(:hotels, [:email])
+      create unique_index(:hotels, [:phone_number])
+      create unique_index(:hotels, [:facebook_url])
+      create unique_index(:hotels, [:instagram_url])
   end
 end
